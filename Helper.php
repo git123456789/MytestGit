d495ff411a93959f2b1935f8e2b54a8dc0feb4ec
git123456789 <123562284@qq.com>
2015-10-12 12:21:26 +0800

    newArticle分支下修改helpert的oArray
    newArticle分支下修改helpert的oArray

--- Helper.php
+++ Helper.php
@@ -2,7 +2,14 @@
 namespace Core/Helper;
 
 class Helper{
-  public function toArray(){}
+  //newArticle分支下修改helpert的oArray
+  public function toArray($array=null){
+    
+    if(is_array($array)){
+      return array_filter($array);
+    }
+    
+  }
   public function toJson(){}
   public function ArrayToJson(){}
   public function JsontoArray(){}
