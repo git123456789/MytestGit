ab0f37dca114d37fbbacf5bf86856ab5487ce801
git123456789 <123562284@qq.com>
2015-10-12 12:19:33 +0800

    在master修改helper文件toArray方法
    master默认分支下修改helpert的oArray

--- Helper.php
+++ Helper.php
@@ -2,7 +2,15 @@
 namespace Core/Helper;
 
 class Helper{
-  public function toArray(){}
+  public function toArray($arr){
+    if(is_array($arr)){
+      return $arr;
+    }elseif(is_object($arr)){
+      return get_object_vars($arr);
+    }else{
+      return null;
+    }
+  }
   public function toJson(){}
   public function ArrayToJson(){}
   public function JsontoArray(){}
