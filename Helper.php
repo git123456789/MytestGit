386d6bd3ce93fd947d299a6d0d07450bc50162ce
git123456789 <123562284@qq.com>
2015-10-12 13:04:58 +0800

    解决分支冲突
    解决分支冲突

--- Helper.php
+++ Helper.php
@@ -3,13 +3,7 @@ namespace Core/Helper;
 
 class Helper{
   //newArticle分支下修改helpert的oArray
-  public function toArray($array=null){
-    
-    if(is_array($array)){
-      return array_filter($array);
-    }
-    
-  }
+  public function toArray(){}
   public function toJson(){}
   public function ArrayToJson(){}
   public function JsontoArray(){}
